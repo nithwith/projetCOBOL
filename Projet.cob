1fee46c5dc6e224184155c9e51c8c14d343cd548
Alexandre <alexandre@MacBook-Air-de-Alexandre.local>
2017-03-16 09:37:40 +0100

    Dernire modif

--- Projet.cob
+++ Projet.cob
@@ -37,7 +37,6 @@ FILE-CONTROL.
        ORGANIZATION INDEXED
        ACCESS MODE IS DYNAMIC
        RECORD KEY fstata_idart
-       ALTERNATE RECORD KEY fstata_classe WITH DUPLICATES
        FILE STATUS IS fstata_stat.
 
        SELECT compteurs ASSIGN TO "compteurs.dat"
@@ -58,11 +57,15 @@ FILE SECTION.
         01 freclTampon.
           02 frecl_id PIC 9(3).
           02 frecl_idco PIC X(6).
-          02 frecl_motif PIC A(20).
+          02 frecl_motif PIC A(40).
           02 frecl_description PIC A(40).
           02 frecl_etat PIC A(10).
           02 frecl_note PIC X(2).
-          02 frecl_date PIC A(15).
+          02 frecl_date_data.
+            05 frecl_date.
+                10 frecl_annee PIC 9(4).
+                10 frecl_mois PIC 9(2).
+                10 frecl_jour PIC 9(2).
 
         FD clients.
         01 fclTampon.
@@ -77,7 +80,11 @@ FILE SECTION.
           02 fco_id PIC 9(3).
           02 fco_idcl PIC 9(3).
           02 fco_idart PIC 9(3).
-          02 fco_date PIC A(15).
+          02 fco_date_data.
+            05 fco_date.
+                10 fco_annee PIC 9(4).
+                10 fco_mois PIC 9(2).
+                10 fco_jour PIC 9(2).
           02 fco_promo PIC X(4).
 
         FD articles.
@@ -92,10 +99,9 @@ FILE SECTION.
         FD statistiquesArticles.
         01 fstataTampon.
           02 fstata_idart PIC 9(3).
-          02 fstata_date PIC A(15).
-          02 fstata_nbrecl PIC X(6).
-          02 fstata_nbCommande PIC X(6).
-          02 fstata_classe PIC A(1).
+          02 fstata_nbrecl PIC 9(3).
+          02 fstata_nbCommande PIC 9(3).
+          02 fstata_pourcent PIC 9(2).
 
 WORKING-STORAGE SECTION.
         77 frecl_stat PIC 9(2).
@@ -103,16 +109,25 @@ WORKING-STORAGE SECTION.
         77 fco_stat PIC 9(2).
         77 fart_stat PIC 9(2).
         77 fstata_stat PIC 9(2).
-        77 fstatc_stat PIC 9(2).
         77 fcpt_stat PIC 9(2).
         77 frecl_idtemp PIC X(6).
         77 Wrep PIC 9.
+        77 Wrep2 PIC 9.
         77 Wfin PIC 9.
+        77 tmp PIC 9(3).
         77 Wmenu PIC 9(1).
         77 Wnom PIC A(20).
         77 Wind PIC 9(6).
         77 W1 PIC 9(2).
         77 tmp_id_article PIC X(6).
+        77 etatRec PIC 9(1).
+        01 SYS-DATE-DATA.
+            05 SYS-DATE .
+                10 SYS-YEAR PIC 9(4).
+                10 SYS-MONTH PIC 9(2).
+                10 SYS-DAY PIC 9(2).
+
+
 
 PROCEDURE DIVISION.
 
@@ -154,14 +169,51 @@ PROCEDURE DIVISION.
                 PERFORM AFFICHAGE_ARTICLE
             END-IF
             IF Wmenu = 9 THEN
-                DISPLAY 'Coucou'
+                PERFORM AFFICHER_STATS_ARTICLES
             END-IF
         END-PERFORM
         STOP RUN.
 
 
 
+        AFFICHER_STATS_ARTICLES.
+            OPEN I-O statistiquesArticles
+            PERFORM WITH TEST AFTER UNTIL Wrep = 0
+            IF fstata_stat = 35 THEN
 
+
+              PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
+                DISPLAY "Pas d'articles, pas de stats" WITH BLANK SCREEN
+                DISPLAY 'Réessayer ? 1 ou 0 :' LINE 2 COL 1
+                ACCEPT Wrep LINE 2 COL 21
+              END-PERFORM
+
+            ELSE
+                DISPLAY "---- Stats Article ----" WITH BLANK SCREEN
+                DISPLAY "Renseigner l'id artcile :" LINE 2 COL 1
+                ACCEPT fstata_idart LINE 2 COL 27
+                READ statistiquesArticles
+                INVALID KEY
+                  PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
+                    DISPLAY "ID article incorrect" WITH BLANK SCREEN
+                    DISPLAY "Réessayer ? 1 ou 0 :" LINE 2 COL 1
+                    ACCEPT Wrep LINE 2 COL 21
+                  END-PERFORM
+                NOT INVALID KEY
+                  MULTIPLY fstata_nbrecl BY 100 GIVING tmp END-MULTIPLY
+    DIVIDE tmp BY fstata_nbCommande GIVING fstata_pourcent END-DIVIDE
+                  REWRITE fstataTampon
+                  DISPLAY "Pourcentage de réclamation :" LINE 3 COL 1
+                  DISPLAY fstata_pourcent LINE 3 COL 30
+                  DISPLAY "%" LINE 3 COL 32
+                  PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
+                    DISPLAY 'Autre article ? 1 ou 0 :' LINE 5 COL 1
+                    ACCEPT Wrep LINE 5 COL 26
+                  END-PERFORM
+                END-READ
+            END-IF
+            END-PERFORM
+            CLOSE statistiquesArticles.
 *> Procédures relatives au fichier réclamation
 
         EFFECTUER_RECLAMATION.
@@ -182,22 +234,29 @@ PROCEDURE DIVISION.
                     OPEN I-O reclamations
             END-IF
 
-            PERFORM WITH TEST AFTER UNTIL Wrep = 0
             OPEN I-O compteurs
-            MOVE 1 TO fcpt_id
-            READ compteurs
-            MOVE fcpt_val TO frecl_id
-            COMPUTE fcpt_val = fcpt_val + 1
-            REWRITE fcptTampon
-            CLOSE compteurs
             OPEN I-O commandes
-            DISPLAY '------ RECLAMATION ------' WITH BLANK SCREEN
-            DISPLAY 'Numero de commande :' LINE 2 COL 1
-            DISPLAY 'Motif de votre reclamation :' LINE 3 COL 1
-            DISPLAY 'Detaillez votre probleme :' LINE 4 COL 1
-            DISPLAY "Date d'aujourd'hui (JJ/MM/AAAA) :" LINE 5 COL 1
+            OPEN I-O statistiquesArticles
+            MOVE 1 TO fcpt_id
 
-            ACCEPT frecl_idco LINE 2 COL 22
+            PERFORM WITH TEST AFTER UNTIL Wrep = 0
+            READ compteurs
+            MOVE FUNCTION CURRENT-DATE TO frecl_date
+
+            DISPLAY '------ RECLAMATION ------' WITH BLANK SCREEN
+            DISPLAY 'Numéro de Réclamation :' LINE 2 COL 1
+            DISPLAY 'Date de la réclamation :' LINE 3 COL 1
+            DISPLAY 'Numero de commande :' LINE 4 COL 1
+            DISPLAY 'Motif de votre reclamation :' LINE 5 COL 1
+            DISPLAY 'Detaillez votre probleme :' LINE 6 COL 1
+            DISPLAY fcpt_val LINE 2 COL 25
+            DISPLAY frecl_jour LINE 3 COL 26
+            DISPLAY '/' LINE 3 COL 28
+            DISPLAY frecl_mois LINE 3 COL 29
+            DISPLAY '/' LINE 3 COL 31
+            DISPLAY frecl_annee LINE 3 COL 32
+
+            ACCEPT frecl_idco LINE 4 COL 22
             MOVE frecl_idco TO fco_id
             READ commandes
             INVALID KEY
@@ -210,24 +269,40 @@ PROCEDURE DIVISION.
 
             NOT INVALID KEY
 
-            ACCEPT frecl_motif LINE 3 COL 30
-            ACCEPT frecl_description LINE 4 COL 28
-            ACCEPT frecl_date LINE 5 COL 35
+            ACCEPT frecl_motif LINE 5 COL 30
+            ACCEPT frecl_description LINE 6 COL 28
             MOVE 'ouvert' TO frecl_etat
-            WRITE freclTampon END-WRITE
-            DISPLAY 'Réclamation réalisée avec succès.' LINE 7 COL 1
-            PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
-              DISPLAY 'Nouvelle reclamation ? 1 ou 0 :' LINE 9 COL 1
-              ACCEPT Wrep LINE 9 COL 33
-            END-PERFORM
-
+            MOVE fcpt_val TO frecl_id
+            COMPUTE fcpt_val = fcpt_val + 1
+            IF fstata_stat = 35 THEN
+                DISPLAY 'Problème statistiquesArticles' LINE 7 COL 1
+                PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
+                  DISPLAY 'Réessayer ? 1 ou 0 :' LINE 9 COL 1
+                  ACCEPT Wrep LINE 9 COL 33
+                END-PERFORM
+            ELSE
+                MOVE fco_idart TO fstata_idart
+                READ statistiquesArticles
+                REWRITE fcptTampon
+                COMPUTE fstata_nbrecl = fstata_nbrecl + 1
+                REWRITE fstataTampon
+                WRITE freclTampon END-WRITE
+                DISPLAY 'Réclamation réalisée avec succès.' LINE 7 COL 1
+                PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
+                  DISPLAY 'Nouvelle reclamation ? 1 ou 0 :' LINE 9 COL 1
+                  ACCEPT Wrep LINE 9 COL 33
+                END-PERFORM
+            END-IF
             END-READ
           END-PERFORM
+          CLOSE statistiquesArticles
+          CLOSE compteurs
           CLOSE commandes
           CLOSE reclamations.
 
           GERER_RECLAMATION.
           OPEN I-O reclamations
+
           IF frecl_stat = 35 THEN
 
             DISPLAY 'Fichier réclamation inexistant' WITH BLANK SCREEN
@@ -247,19 +322,54 @@ PROCEDURE DIVISION.
                 DISPLAY 'Réclamation inexistante' LINE 3 COL 1
             NOT INVALID KEY
                DISPLAY 'Id reclamation :' LINE 3 COL 1
-               DISPLAY frecl_id LINE 3 COL 14
+               DISPLAY frecl_id LINE 3 COL 16
                DISPLAY 'Id commande :' LINE 4 COL 1
                DISPLAY frecl_idco LINE 4 COL 15
                DISPLAY 'Motif :' LINE 5 COL 1
-               DISPLAY frecl_motif LINE 5 COL 8
+               DISPLAY frecl_motif LINE 5 COL 9
                DISPLAY 'Etat :' LINE 6 COL 1
-               DISPLAY frecl_etat LINE 6 COL 18
+               DISPLAY frecl_etat LINE 6 COL 8
                DISPLAY 'Date :' LINE 7 COL 1
-               DISPLAY frecl_date LINE 7 COL 18
+               DISPLAY frecl_jour LINE 7 COL 8
+               DISPLAY '/' LINE 7 COL 10
+               DISPLAY frecl_mois LINE 7 COL 11
+               DISPLAY '/' LINE 7 COL 13
+               DISPLAY frecl_annee LINE 7 COL 14
             END-READ
+            PERFORM WITH TEST AFTER UNTIL Wrep2 = 0 OR Wrep2 = 1
+                DISPLAY 'Modifier cette réclamation :' LINE 9 COL 1
+                DISPLAY '1 --> OUI' LINE 10 COL 1
+                DISPLAY '0 --> NON' LINE 11 COL 1
+                ACCEPT Wrep2 LINE 12 COL 1
+            END-PERFORM
+            IF Wrep2 = 1 THEN
+          DISPLAY "---- Modification Réclamation ----" WITH BLANK SCREEN
+              DISPLAY 'ID Réclamation modifiée :' LINE 3 COL 1
+              DISPLAY frecl_id LINE 3 COL 26
+              DISPLAY 'Nouvel Etat de la réclamation :' LINE 4 COL 1
+              DISPLAY '1 --> ouvert' LINE 5 COL 1
+              DISPLAY '2 --> en traitement' LINE 6 COL 1
+              DISPLAY '3 --> cloture' LINE 7 COL 1
+              DISPLAY 'Autre --> ne rien faire' LINE 8 COL 1
+              DISPLAY 'Choix :' LINE 9 COL 1
+              ACCEPT etatRec LINE 9 COL 9
+              IF etatRec = 1 THEN
+                MOVE 'ouvert' TO frecl_etat
+                REWRITE freclTampon
+              END-IF
+              IF etatRec = 2 THEN
+                MOVE 'traitement' TO frecl_etat
+                REWRITE freclTampon
+              END-IF
+              IF etatRec = 3 THEN
+                MOVE 'cloture' TO frecl_etat
+                REWRITE freclTampon
+              END-IF
+            END-IF
+
             PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
-             DISPLAY 'Afficher une autre recl ? 1 ou 0 :' LINE 8 COL 1
-             ACCEPT Wrep LINE 8 COL 34
+             DISPLAY 'Traiter une autre recl ? 1 ou 0 :' LINE 14 COL 1
+             ACCEPT Wrep LINE 14 COL 35
             END-PERFORM
           END-PERFORM
           END-IF
@@ -268,6 +378,7 @@ PROCEDURE DIVISION.
 *> Procédures relatives au fichier clients
 
         AJOUT_CLIENT.
+
         OPEN I-O clients
         IF fcl_stat = 35 THEN
                 OPEN OUTPUT clients
@@ -284,30 +395,33 @@ PROCEDURE DIVISION.
                 CLOSE compteurs
                 OPEN I-O clients
         END-IF
-
-        PERFORM WITH TEST AFTER UNTIL Wrep = 0
         OPEN I-O compteurs
         MOVE 2 TO fcpt_id
+
+        PERFORM WITH TEST AFTER UNTIL Wrep = 0
         READ compteurs
-        MOVE fcpt_val TO fcl_id
-        COMPUTE fcpt_val = fcpt_val + 1
-        REWRITE fcptTampon
-        CLOSE compteurs
         DISPLAY '------- AJOUT CLIENT -------' WITH BLANK SCREEN
+          DISPLAY 'Identifiant Client :' LINE 2 COL 1
           DISPLAY 'Nom Client :' LINE 3 COL 1
           DISPLAY 'Prenom Client :' LINE 4 COL 1
           DISPLAY 'Telephone Client :' LINE 5 COL 1
           DISPLAY 'Mail client :' LINE 6 COL 1
+          DISPLAY fcpt_val LINE 2 COL 22
           ACCEPT fcl_nom LINE 3 COL 14
           ACCEPT fcl_prenom LINE 4 COL 17
           ACCEPT fcl_tel LINE 5 COL 20
           ACCEPT fcl_mail LINE 6 COL 15
+          MOVE fcpt_val TO fcl_id
+          COMPUTE fcpt_val = fcpt_val + 1
+          REWRITE fcptTampon
+
           WRITE fclTampon END-WRITE
           PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
              DISPLAY 'Ajouter un autre client ? 1 ou 0 :' LINE 8 COL 1
              ACCEPT Wrep LINE 9 COL 1
           END-PERFORM
         END-PERFORM
+        CLOSE compteurs
         CLOSE clients.
 
         AFFICHER_CLIENT.
@@ -368,30 +482,33 @@ PROCEDURE DIVISION.
                   CLOSE compteurs
                   OPEN I-O commandes
           END-IF
-
-          PERFORM WITH TEST AFTER UNTIL Wrep = 0
-          DISPLAY '------- AJOUT COMMANDE -------' WITH BLANK SCREEN
-
-          DISPLAY 'Identifiant Client :' LINE 3 COL 1
-          DISPLAY 'Identifiant Article :' LINE 4 COL 1
-          DISPLAY 'Date de commande (JJ/MM/AAAA): ' LINE 5 COL 1
-          DISPLAY 'Code Promo :' LINE 6 COL 1
-
           OPEN I-O compteurs
-          MOVE 3 TO fcpt_id
-          READ compteurs
-          MOVE fcpt_val TO fco_id
-          COMPUTE fcpt_val = fcpt_val + 1
-          REWRITE fcptTampon
-          CLOSE compteurs
-
           OPEN I-O clients
+          OPEN I-O statistiquesArticles
           OPEN I-O articles
+          MOVE 3 TO fcpt_id
+          PERFORM WITH TEST AFTER UNTIL Wrep = 0
 
-          ACCEPT fco_idcl LINE 3 COL 22
+          READ compteurs
+          MOVE FUNCTION CURRENT-DATE TO fco_date
+
+          DISPLAY '------- AJOUT COMMANDE -------' WITH BLANK SCREEN
+          DISPLAY 'Identifiant de la commande :' LINE 2 COL 1
+          DISPLAY 'Date de commande :' LINE 3 COL 1
+          DISPLAY 'Identifiant Client :' LINE 4 COL 1
+          DISPLAY 'Identifiant Article :' LINE 5 COL 1
+          DISPLAY 'Code Promo :' LINE 6 COL 1
+          DISPLAY fcpt_val LINE 2 COL 30
+
+
+          DISPLAY fco_jour LINE 3 COL 20
+          DISPLAY '/' LINE 3 COL 22
+          DISPLAY fco_mois LINE 3 COL 23
+          DISPLAY '/' LINE 3 COL 25
+          DISPLAY fco_annee LINE 3 COL 26
+          ACCEPT fco_idcl LINE 4 COL 22
           MOVE fco_idcl TO fcl_id
           READ clients
-
           INVALID KEY
 
             DISPLAY 'Client inexistant' WITH BLANK SCREEN
@@ -402,7 +519,7 @@ PROCEDURE DIVISION.
 
           NOT INVALID KEY
 
-          ACCEPT fco_idart LINE 4 COL 23
+          ACCEPT fco_idart LINE 5 COL 23
           MOVE fco_idart TO fart_id
           READ articles
 
@@ -414,16 +531,25 @@ PROCEDURE DIVISION.
                 END-PERFORM
 
               NOT INVALID KEY
-                ACCEPT fco_date LINE 5 COL 32
+                MOVE fart_id TO fstata_idart
+                READ statistiquesArticles
                 ACCEPT fco_promo LINE 6 COL 14
+                MOVE fcpt_val TO fco_id
+                COMPUTE fstata_nbCommande = fstata_nbCommande + 1
+                REWRITE fstataTampon
+                COMPUTE fcpt_val = fcpt_val + 1
+                REWRITE fcptTampon
                 WRITE fcoTampon END-WRITE
                 PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
                    DISPLAY 'Nouvelle commande ? 1 ou 0 :' LINE 8 COL 1
                    ACCEPT Wrep LINE 8 COL 30
                 END-PERFORM
-          END-READ
+
+                END-READ
           END-READ
           END-PERFORM
+          CLOSE compteurs
+          CLOSE statistiquesArticles
           CLOSE clients
           CLOSE articles
           CLOSE commandes.
@@ -432,12 +558,15 @@ PROCEDURE DIVISION.
           AFFICHER_COMMANDE.
           OPEN I-O commandes
           IF fco_stat = 35 THEN
+
             DISPLAY 'Fichier commandes inexistant' WITH BLANK SCREEN
             PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
                DISPLAY 'Réessayer ? 1 ou 0 :' LINE 2 COL 1
                ACCEPT Wrep LINE 2 COL 22
             END-PERFORM
+
           ELSE
+
           PERFORM WITH TEST AFTER UNTIL Wrep = 0
           DISPLAY '------- Recherche Commande -------' WITH BLANK SCREEN
           DISPLAY "Identifiant de la commande :" LINE 2 COL 1
@@ -453,7 +582,11 @@ PROCEDURE DIVISION.
              DISPLAY 'Article :' LINE 5 COL 1
              DISPLAY fco_idart LINE 5 COL 11
              DISPLAY 'Date :' LINE 6 COL 1
-             DISPLAY fco_date LINE 6 COL 8
+             DISPLAY fco_jour LINE 6 COL 8
+             DISPLAY '/' LINE 6 COL 10
+             DISPLAY fco_mois LINE 6 COL 11
+             DISPLAY '/' LINE 6 COL 13
+             DISPLAY fco_annee LINE 6 COL 14
              DISPLAY 'Code Promo utilisé :' LINE 7 COL 1
              DISPLAY fco_promo LINE 7 COL 22
          END-READ
@@ -484,32 +617,55 @@ PROCEDURE DIVISION.
                   CLOSE compteurs
                   OPEN I-O articles
           END-IF
-          PERFORM WITH TEST AFTER UNTIL Wrep = 0
-          DISPLAY '------- AJOUT ARTICLE -------' WITH BLANK SCREEN
 
+          OPEN I-O statistiquesArticles
+          IF fstata_stat = 35 THEN
+            OPEN OUTPUT statistiquesArticles
+            CLOSE statistiquesArticles
+            OPEN I-O statistiquesArticles
+          END-IF
+
+
+          OPEN I-O compteurs
+          MOVE 4 TO fcpt_id
+          PERFORM WITH TEST AFTER UNTIL Wrep = 0
+
+          READ compteurs
+
+          DISPLAY '------- AJOUT ARTICLE -------' WITH BLANK SCREEN
+          DISPLAY 'Identifiant article :' LINE 2 COL 1
           DISPLAY 'Nom article :' LINE 3 COL 1
           DISPLAY 'Type article :' LINE 4 COL 1
           DISPLAY 'Description article :' LINE 5 COL 1
           DISPLAY 'Prix article :' LINE 6 COL 1
           DISPLAY 'Duree garantie :' LINE 7 COL 1
-          OPEN I-O compteurs
-          MOVE 4 TO fcpt_id
-          READ compteurs
-          MOVE fcpt_val TO fart_id
-          COMPUTE fcpt_val = fcpt_val + 1
-          REWRITE fcptTampon
-          CLOSE compteurs
+
+          DISPLAY fcpt_val LINE 2 COL 23
+
+
+
           ACCEPT fart_nom LINE 3 COL 15
           ACCEPT fart_type LINE 4 COL 16
           ACCEPT fart_description LINE 5 COL 23
           ACCEPT fart_prix LINE 6 COL 16
           ACCEPT fart_dureegaranti LINE 7 COL 18
+          MOVE fcpt_val TO fart_id
+          MOVE fcpt_val TO fstata_idart
+          MOVE 0 TO fstata_pourcent
+          MOVE 0 TO fstata_nbCommande
+          MOVE 0 TO fstata_nbrecl
+          COMPUTE fcpt_val = fcpt_val + 1
+
+          REWRITE fcptTampon
+          WRITE fstataTampon END-WRITE
           WRITE fartTampon END-WRITE
           PERFORM WITH TEST AFTER UNTIL Wrep = 0 OR Wrep = 1
              DISPLAY 'Ajouter un autre article ? 1 ou 0 :' LINE 8 COL 1
-             ACCEPT Wrep LINE 8 COL 36
+             ACCEPT Wrep LINE 8 COL 37
           END-PERFORM
           END-PERFORM
+          CLOSE compteurs
+          CLOSE statistiquesArticles
           CLOSE articles.
 
         AFFICHAGE_ARTICLE.
